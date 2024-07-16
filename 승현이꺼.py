fdfe14a6552f307b6be87ee0b3c32fcf8f126c84
Jasddf <jangbee9610@naver.com>
2024-07-17 08:45:19 +0900

    Create 승현이꺼.py

---
+++ 승현이꺼.py
@@ -0,0 +1,10 @@
+import random
+
+result=[]
+while int(len(result))<6:
+    new = random.randint(1,45)
+    if new in result :
+        print('중복')
+    else :
+        result.append(new)
+print(result)
